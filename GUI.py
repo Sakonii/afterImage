190429ec8ad61afbcbf336b629cc0262918d989b
Utsav Maskey <skelliger7@Skelliger7.local>
2019-12-13 22:46:48 +0545

    gui-object-deletion

--- GUI.py
+++ GUI.py
@@ -14,11 +14,13 @@ class ImageObjects:
         self.contours_list = []
 
         self.img_display = None
-        self.obj_selected = None
+        self.key_pressed = None
+
+        self.obj_selected = []
 
         # This will be the default window for inference
-        cv2.namedWindow("Image Objects")
-        self.boolSegment = cv2.createTrackbar(
+        cv2.namedWindow(winname="Image Objects")
+        self.bool_segment = cv2.createTrackbar(
             "Show Segments", "Image Objects", 0, 1, self._show_objects
         )
         cv2.setMouseCallback("Image Objects", self.mouse_events)
@@ -26,21 +28,149 @@ class ImageObjects:
     def contour_containing_point(self, x, y):
         "Returns the contour under which the co-ordinate falls"
 
-        for Class in range(self.num_classes):
-            contours = self.contours_list[Class]
-
+        for contours in self.contours_list:
             for contour in contours:
-                if cv2.pointPolygonTest(contour, (x, y), False) >= 0:
+                if (
+                    cv2.pointPolygonTest(contour=contour, pt=(x, y), measureDist=False)
+                    >= 0
+                    and contour is not self.obj_selected
+                ):
                     return contour
+                else:
+                    pass
+        return []
 
-        return None
+    def show_mask(self):
+        "Displays Segmentation Mask"
+
+        cv2.imshow(winname="Image Objects", mat=self.mask)
+
+        if cv2.waitKey(0) & 0xFF == ord("q"):
+            cv2.destroyAllWindows()
+
+    def detect_objects(self):
+        "Updates the list of contours of detected image objects"
+
+        # Iterate over every class and find its corresponding image object as a list of contours
+        for _class in range(self.num_classes):
+            mask_temp = cv2.inRange(
+                src=self.mask, lowerb=np.array([_class]), upperb=np.array([_class])
+            )
+            contours_temp, _ = cv2.findContours(
+                image=mask_temp, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_NONE
+            )
+            self.contours_list.append(contours_temp)
+
+    def draw_objects(self, showSegmentation=False, onBlank=False, fromClean=False):
+        "Returns np.ndarray image bounded by detected image object's contours"
+
+        if onBlank:
+            # Create a null matrix that matches the size of output
+            img_shape = self.mask.shape
+            self.img_display = np.zeros(shape=img_shape)
+        else:
+            self.img_display = np.copy(self.img if fromClean else self.img_display)
+
+        if showSegmentation:
+            for _class in range(self.num_classes):
+                cv2.drawContours(
+                    image=self.img_display,
+                    contours=self.contours_list[_class],
+                    contourIdx=-1,
+                    color=(75, 120, 75),
+                    thickness=2,
+                    lineType=cv2.LINE_AA,
+                )
+
+        # Draws detected objects onto img_display
+        cv2.drawContours(
+            image=self.img_display,
+            contours=self.obj_selected,
+            contourIdx=-1,
+            color=(0, 215, 255),
+            thickness=2,
+            lineType=cv2.LINE_AA,
+        )
+        return self.img_display
+
+    def show_objects(self, fromClean=False):
+        "Displays Image Objects"
+
+        self.draw_objects(self.bool_segment, fromClean=fromClean)
+        cv2.imshow(winname="Image Objects", mat=self.img_display)
+
+    def _show_objects(self, x=None):
+        "Function Wrapper to switch Segmentation view (only for trackbar)"
+
+        self.bool_segment = not self.bool_segment
+        self.draw_objects(self.bool_segment, fromClean=True)
+        cv2.imshow(winname="Image Objects", mat=self.img_display)
+
+    def obj_delete(self, inPlace=True):
+        "Delete the selected object"
+        self.img_display = np.copy(self.img)
+
+        cv2.drawContours(
+            image=self.img_display,
+            contours=[self.obj_selected],
+            contourIdx=-1,
+            color=(255, 255, 255),
+            thickness=cv2.FILLED,
+        )
+        if inPlace:
+            self.img_save()
+
+    def img_save(self):
+        "Save display image 'self.img_display' to actual image 'self.img' "
+        self.obj_selected = []
+        self.img = self.img_display
+
+    def img_reload(self):
+        "Re-loads display image from actual image"
+        self.img_display = np.copy(self.img)
+        self.obj_selected = []
+        self.show_objects(fromClean=False)
+
+    def keyboard_events(self):
+        "Non-callback keyboard input function for image manipulation"
+
+        while True:
+            self.key_pressed = cv2.waitKey(0)
+
+            if self.key_pressed & 0xFF == ord("q"):
+                cv2.destroyAllWindows()
+                return
+
+            elif self.key_pressed & 0xFF == ord("r"):
+                self.img_reload()
+                continue
+
+            if self.obj_selected != []:
+                # If any of the object is selected by mouse event
+                if self.key_pressed & 0xFF == ord("d"):
+                    self.obj_delete()
+                    self.show_objects(fromClean=False)
+                    print("deleted")
+
+                elif self.key_pressed & 0xFF == ord("m"):
+                    print("moving")
+
+                elif self.key_pressed & 0xFF == ord("s"):
+                    print("Can't save. Un-select the object first.")
+
+            elif self.obj_selected == []:
+                # If none of the object is selected
+                if self.key_pressed & 0xFF == ord("s"):
+                    self.img_save()
+                else:
+                    print(f"Trigger a defined keyboard or mouse event")
 
     def mouse_events(self, event, x, y, flags, param):
         "Mouse callback function definition"
 
         if event == cv2.EVENT_LBUTTONDOWN:
             self.obj_selected = self.contour_containing_point(x, y)
-            self.show_objects()
+            self.show_objects(fromClean=True)
 
         elif event == cv2.EVENT_MOUSEMOVE:
             "Later"
@@ -48,62 +178,8 @@ class ImageObjects:
         elif event == cv2.EVENT_LBUTTONUP:
             "Later"
 
-    def show_mask(self):
-        "Displays Segmentation Mask"
-
-        cv2.imshow("Image Objects", self.mask)
-
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-
-    def detect_objects(self):
-        "Updates the list of contours of detected image objects"
-
-        # Iterate over every class and find its corresponding image object as a list of contours
-        for Class in range(self.num_classes):
-            mask_temp = cv2.inRange(self.mask, np.array([Class]), np.array([Class]))
-            contours_temp, _ = cv2.findContours(
-                mask_temp, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE
-            )
-            self.contours_list.append(contours_temp)
-
-    def draw_objects(self, showSegmentation=False, onBlank=False):
-        "Returns np.ndarray image bounded by detected image object's contours"
-
-        if onBlank:
-            # Create a null matrix that matches the size of output
-            img_shape = self.mask.shape
-            self.img_display = np.zeros(img_shape)
-        else:
-            # Draws detected objects onto img_display
-            self.img_display = np.copy(self.img)
-
-        if showSegmentation:
-            for Class in range(self.num_classes):
-                cv2.drawContours(
-                    self.img_display, self.contours_list[Class], -1, (75, 120, 75), 1
-                )
-
-        cv2.drawContours(self.img_display, self.obj_selected, -1, (0, 215, 255), 1)
-        return self.img_display
-
-    def show_objects(self):
-        "Displays Image Objects"
-
-        self.draw_objects(self.boolSegment)
-        cv2.imshow("Image Objects", self.img_display)
-
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-
-    def _show_objects(self, x=None):
-        "Function Wrapper to switch Segmentation view"
-
-        self.boolSegment = not self.boolSegment
-        self.draw_objects(self.boolSegment)
-        cv2.imshow("Image Objects", self.img_display)
-
     def inference(self):
         "Run Image Manipulation inference"
         self.detect_objects()
-        self.show_objects()
+        self.show_objects(fromClean=True)
+        self.keyboard_events()
